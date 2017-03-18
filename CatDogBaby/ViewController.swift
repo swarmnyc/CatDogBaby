d30cb22eb06c483f423a804b117fec2458b12683
wsrobins <wsrobins@uvm.edu>
2017-03-18 00:02:29 -0400

    Add buttons and speech to main view

--- CatDogBaby/ViewController.swift
+++ CatDogBaby/ViewController.swift
@@ -7,19 +7,85 @@
 //
 
 import UIKit
+import SnapKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+	// Subviews
+	lazy var catButton: UIButton = {
+		let button = UIButton(type: .custom)
+		button.setTitle("cat", for: .normal)
+		button.titleLabel!.font = UIFont.systemFont(ofSize: 50, weight: UIFontWeightBlack)
+		button.titleLabel!.layer.shadowRadius = 20
+		button.titleLabel!.layer.shadowOffset = CGSize(width: 0, height: 0)
+		button.titleLabel!.layer.shadowOpacity = 0.2
+		button.setBackgroundImage(#imageLiteral(resourceName: "TubeCat"), for: .normal)
+		button.imageView!.contentMode = .scaleAspectFill
+		button.addTarget(self, action: #selector(catButtonTouched), for: .touchUpInside)
+		return button
+	}()
+	lazy var dogButton: UIButton = {
+		let button = UIButton()
+		button.setTitle("dog", for: .normal)
+		button.titleLabel!.font = UIFont.systemFont(ofSize: 50, weight: UIFontWeightBlack)
+		button.titleLabel!.layer.shadowRadius = 20
+		button.titleLabel!.layer.shadowOffset = CGSize(width: 0, height: 0)
+		button.titleLabel!.layer.shadowOpacity = 0.2
+		button.setBackgroundImage(#imageLiteral(resourceName: "HappyDog"), for: .normal)
+		button.imageView!.contentMode = .scaleAspectFill
+		button.addTarget(self, action: #selector(dogButtonTouched), for: .touchUpInside)
+		return button
+	}()
+	lazy var babyButton: UIButton = {
+		let button = UIButton()
+		button.setTitle("baby", for: .normal)
+		button.titleLabel!.font = UIFont.systemFont(ofSize: 50, weight: UIFontWeightBlack)
+		button.titleLabel!.layer.shadowRadius = 20
+		button.titleLabel!.layer.shadowOffset = CGSize(width: 0, height: 0)
+		button.titleLabel!.layer.shadowOpacity = 0.2
+		button.setBackgroundImage(#imageLiteral(resourceName: "ConfusedBaby"), for: .normal)
+		button.imageView!.contentMode = .scaleAspectFill
+		button.addTarget(self, action: #selector(babyButtonTouched), for: .touchUpInside)
+		return button
+	}()
+	
+	// Speech
+	lazy var speechSynthesizer: AVSpeechSynthesizer = AVSpeechSynthesizer()
+	
+	// Life cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		view.addSubview(catButton)
+		view.addSubview(dogButton)
+		view.addSubview(babyButton)
+		catButton.snp.makeConstraints {
+			$0.top.left.right.equalToSuperview()
+			$0.height.equalToSuperview().dividedBy(3)
+		}
+		dogButton.snp.makeConstraints {
+			$0.top.equalTo(catButton.snp.bottom)
+			$0.left.right.equalToSuperview()
+			$0.bottom.equalTo(babyButton.snp.top)
+		}
+		babyButton.snp.makeConstraints {
+			$0.left.bottom.right.equalToSuperview()
+			$0.height.equalToSuperview().dividedBy(3)
+		}
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	// Actions
+	func catButtonTouched() {
+		speechSynthesizer.
+		speechSynthesizer.speak(AVSpeechUtterance(string: "Yep that's a cat!"))
 	}
-
-
+	func dogButtonTouched() {
+		speechSynthesizer.pauseSpeaking(at: .immediate)
+		speechSynthesizer.speak(AVSpeechUtterance(string: "This animal is a dog!"))
+	}
+	func babyButtonTouched() {
+		speechSynthesizer.pauseSpeaking(at: .immediate)
+		speechSynthesizer.speak(AVSpeechUtterance(string: "That is one fat baby..."))
+	}
 }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Global platform
+platform :ios, '9.3'
+
+# App targets
+target 'CatDogBaby' do
+  use_frameworks!
+  pod 'SnapKit'
+  pod 'CameraEngine'
+
+  # Test targets
+  target 'CatDogBabyTests' do
+    inherit! :search_paths
+  end
+end
